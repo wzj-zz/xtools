1efacbb0a37fb5874610b3ff0a63d04680099468
wzj_re <wzj_reverse@outlook.com>
2022-11-09 09:44:36 +0800

    delete: rop.bat rop.py

--- bin/Lib/rop.py
+++
@@ -1,2 +0,0 @@
-import ropgadget
-ropgadget.main()
--- bin/rop.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%1"=="" (
-	p %~dp0Lib\rop.py --console
-) else if "%1"=="-h" (
-	p %~dp0Lib\rop.py --help
-) else (
-	p %~dp0Lib\rop.py --binary %*
-)
-@echo on
